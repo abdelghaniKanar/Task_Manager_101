0936ee5ed97af8630685ce8c8701c16538436306
kanarAb <KanarAb.webapps001@gmail.com>
2024-10-30 15:22:07 +0100

    coding 'menu' function

--- Task_Manager_101/main.c
+++ Task_Manager_101/main.c
@@ -1,50 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 //Define the date structure to be used as a nested structure.
-typedef struct {
+typedef struct
+{
 
     int day, month, year;
 
-}date;
+} date;
 
-//Define the main structure "task".
-typedef struct {
+//Define the main structure "task"
+typedef struct
+{
+    char title[25]; //25 characters for title, it's enough!!!
 
-    char title[25]; //25 characters for title, I think it's enough.
+    char description[350]; //Expect the description to contain 350 characters or fewer
 
-    char description[350]; //I expect that the description should contain 350 characters or less.
+    char priority[25]; //High or low.
 
-    char priority[25]; //high or low.
+    date dueDate; //The dueDate is the date on which the user expects this task to be completed
 
-    date dueDate; //The dueDate is the date on which the user expects this task to be completed.
+} task;
 
-}task;
+//Declaring the Global Variables
+int taskListLength, cheker = 0;
+task tasks[];
 
-//Define the "menu" function
+
+//Declaring the "menu" function
 void menu();
 
-//Define the "addTask" function
+//Declaring the "addTask" function
 void addTask();
 
-//Define the "displayTask" function
+//Declaring the "displayTask" function
 void displayTask();
 
-//Define the "filter" function to be used within "displayTask" for full display features.
+//Declaring the "filter" function to be used within "displayTask" for full display features
 void filter();
 
-//Define the "updateTask" function
+//Declaring the "updateTask" function
 void updateTask();
 
-//Define the "deleteTask" function
+//Declaring the "deleteTask" function
 void deleteTask();
 
 int main()
 {
+    printf("###################################################\n");
+    printf("############### Task Manager 101' #################\n");
+    printf("###################################################\n");
 
-    printf("---------------------------------------------------\n");
-    printf("--------------- Task Manager 101' -----------------\n");
-    printf("---------------------------------------------------\n");
+    menu();
 
     return 0;
+}
+
+//Define the "menu" function
+void menu()
+{
+    char choice;
+
+    do
+    {
+        printf("\n------------------ Control Menu -------------------\n");
+        printf("------------ You Had One Choice to Make -----------\n");
+        //Possible options
+        printf("\n'A' or 'a': Add a new task.\n");
+        printf("'D' or 'd': View your tasks.\n");
+        printf("'U' or 'u': Update an existing task.\n");
+        printf("'R' or 'r': Remove a task from your list.\n");
+        printf("'O' or 'o': Exit the application.\n");
+        printf("\nPlease enter your choice: ");
+        scanf("%s", &choice);
+
+        //Here is the verification section to verify the user's chosen option
+        switch (toupper(choice))
+        {
+
+        case 'A':
+            if(cheker == 0)
+            {
+
+                printf("\nOops! We still work on this section!\n");
+
+            }
+            break;
+
+        case 'D':
+            if(cheker == 0)
+            {
+
+                printf("\nOops! There is no task to display!\n");
+
+            }
+            break;
+
+        case 'U':
+            if(cheker == 0)
+            {
+
+                printf("\nOops! There is no task to update!\n");
+
+            }
+            break;
+
+        case 'R':
+            if(cheker == 0)
+            {
+
+                printf("\nOops! There is no task to remove!\n");
+
+            }
+            break;
+
+        case 'O':
+            break;
+
+
+        }
+
+    }
+    while ( choice != 'o' );
+
 }
